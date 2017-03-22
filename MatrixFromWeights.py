19e9268a394c89210fc5c4e37724f16ec02b0d4a
KamChaos <kam.chaos@gmail.com>
2017-03-22 20:17:59 +0300

    one triplet

--- MatrixFromWeights.py
+++ MatrixFromWeights.py
@@ -10,6 +10,13 @@ import array
 #from scipy.fftpack import fft, ifft
 
 
+def random_unit_vector():
+    phi = 2.0 * math.pi * np.random.random()
+    z = 2.0 * np.random.random() - 1.0
+    r = math.sqrt(1.0 - z * z)
+    return np.array([r * math.cos(phi), r * math.sin(phi), z])
+
+
 class Rotation:
     """
     * Rotation : provides a representation for 3D space rotations
@@ -91,6 +98,7 @@ class Rotation:
             2.0 * np.random.random() - 1.0), 2. * math.pi * np.random.random()
         self.euler_angles(V)
 
+
 class TripletHamiltonian:
     def __init__(self):
         self.Id = np.matrix('1 0 0; 0 1 0; 0 0 1', dtype=np.complex_)
@@ -120,12 +128,100 @@ class TripletHamiltonian:
 
     def eval(self, D, E, B, theta=0, phi=0, mol_basis=True):
         if mol_basis:
-            return np.linalg.eig(self.spin_hamiltonian_mol_basis(D, E, B, theta, phi))
+            return np.linalg.eigvalsh(self.spin_hamiltonian_mol_basis(D, E, B, theta, phi))
         else:
-            return np.linalg.eig(self.spin_hamiltonian_field_basis(D, E, B, theta, phi))
+            return np.linalg.eigvalsh(self.spin_hamiltonian_field_basis(D, E, B, theta, phi))
 
-################################################
-#ExpData Plot Sam's approach
+class ODMR_Signal:
+    """
+    * ODMR_Signal
+    *
+    * Output : Computes ODMR and magnetic resonance signals
+    *
+    * Input : spins, a reference on SpinSystem object
+    * SpinSystem should define
+    * spins.matrix_size
+    * spins.evec
+    * spins.eval
+    * spins.singlet_projector()
+    * spins.Bac_field_basis_matrix()
+    """
+
+    def __init__(self, spin_system):
+        self.spins = spin_system
+        self.rho0 = np.empty(self.spins.matrix_size, dtype=float)
+        self.rho2 = np.empty([self.spins.matrix_size, self.spins.matrix_size], dtype=np.complex_)
+        self.gamma = None
+        self.gamma_diag = None
+
+    def update_from_spin_hamiltonian(self):
+        self.Sproj_eig_basis = reduce(np.dot, [np.matrix.getH(self.spins.evec), self.spins.singlet_projector(),
+                                               self.spins.evec])
+        self.V = reduce(np.dot, [np.matrix.getH(self.spins.evec), self.spins.Bac_field_basis_matrix(), self.spins.evec])
+
+    def omega_nm(self, n, m):
+        return self.spins.eval[n] - self.spins.eval[m]
+
+    def load_rho0_thermal(self, Temp):
+        sum = 0
+        for i in range(self.spins.matrix_size):
+            rho0_i = math.exp(- self.spins.eval[i] / Temp)
+            self.rho0[i] = rho_i
+            sum += rho_i
+        self.rho0 /= sum
+
+    def load_rho0_from_singlet(self):
+        sum = 0
+        for i in range(self.spins.matrix_size):
+            self.rho0[i] = self.Sproj_eig_basis[i, i].real
+            sum += self.rho0[i]
+        self.rho0 /= sum
+
+    def chi1(self, omega):
+        c1 = 0j
+        for m in range(self.spins.matrix_size):
+            for n in range(self.spins.matrix_size):
+                # the contribution to chi1 vanishes for n == m, whether gamma is the same for diagonal and non diagonal elements is not relvant here
+                Vmn = self.V[m, n]
+                Vmn_abs2 = Vmn.real * Vmn.real + Vmn.imag * Vmn.imag
+                c1 -= (self.rho0[m] - self.rho0[n]) * Vmn_abs2 / (self.omega_nm(n, m) - omega - 1j * self.gamma);
+        return c1
+
+    def find_rho2_explicit(self, omega):
+        for m in range(self.spins.matrix_size):
+            for n in range(self.spins.matrix_size):
+                rrr = 0j
+                for nu in range(self.spins.matrix_size):
+                    for p in [-1., 1.]:
+                        gamma_nm = self.gamma_diag if m == n else self.gamma
+                        rrr += (self.rho0[m] - self.rho0[nu]) * self.V[n, nu] * self.V[nu, m] / (
+                        (self.omega_nm(n, m) - 1j * gamma_nm) * (self.omega_nm(nu, m) - omega * p - 1j * self.gamma))
+                        rrr -= (self.rho0[nu] - self.rho0[n]) * self.V[n, nu] * self.V[nu, m] / (
+                        (self.omega_nm(n, m) - 1j * gamma_nm) * (self.omega_nm(n, nu) - omega * p - 1j * self.gamma))
+                self.rho2[n, m] = rrr
+
+    def find_rho2(self, omega):
+        Vtmp = np.zeros((self.spins.matrix_size, self.spins.matrix_size), dtype=np.complex_)
+        for m in range(self.spins.matrix_size):
+            for nu in range(self.spins.matrix_size):
+                for p in [-1., 1.]:
+                    Vtmp[nu, m] += (self.rho0[m] - self.rho0[nu]) * self.V[nu, m] / (
+                    self.omega_nm(nu, m) - omega * p - 1j * self.gamma)
+        self.rho2 = np.dot(self.V, Vtmp) - np.dot(Vtmp, self.V)
+        for m in range(self.spins.matrix_size):
+            for n in range(self.spins.matrix_size):
+                gamma_nm = self.gamma_diag if m == n else self.gamma
+                self.rho2[n, m] /= (self.omega_nm(n, m) - 1j * gamma_nm);
+
+    def odmr(self, omega):
+        odmr_amp = 0j
+        self.find_rho2(omega)
+
+        for m in range(self.spins.matrix_size):
+            for n in range(self.spins.matrix_size):
+                odmr_amp += self.rho2[m, n] * self.Sproj_eig_basis[n, m]
+
+        return odmr_amp.real
 
 
 dataDC2 = np.loadtxt("testupto30up.txt", comments='%')#, usecols=(0,1,3),unpack=True)
@@ -183,7 +279,7 @@ for trp.phi in Phi:
         Theta_deg[index_Theta] = round(math.degrees(Theta[index_Theta]))
         for i in range(len(freqDC2)):
             for trp.B in Magnetic:
-                vals, vecs = trp.eval(trp.D, trp.E, trp.B, trp.theta, trp.phi, mol_basis=True)
+                vals = trp.eval(trp.D, trp.E, trp.B, trp.theta, trp.phi, mol_basis=True)
                 x1 = (vals[1].real - vals[0].real)
                 x2 = (vals[2].real - vals[0].real)
                 L1 = freqDC2[i] - x1
